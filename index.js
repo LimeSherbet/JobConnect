adc98264ad1c163a27ae922bb568f4f31312a79c
John Gaskell <john.liam.gaskell@gmail.com>
2024-04-23 04:27:07 +0100

    got it querying the database on request

--- index.js
+++ index.js
@@ -2,11 +2,43 @@ var http = require('http');
 var fs = require('fs');
 url = require('url');
 express = require('express');
-app = express();
+mysql = require('mysql');
+path = require('path');
+app = express(); 
 
-http.createServer(function (req, res) {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end(fs.readFileSync('./web/src/index.html', 'utf8'));
+var publicFolder = path.join(__dirname, './web/dist/web/browser/')
+app.use(express.static(publicFolder))
+app.use(express.json());
 
-}).listen(4200);
+app.listen(4201, function(err){
+    if (err) console.log("Error in server setup")
+    console.log("Server listening on Port", 4200);
+    
+})
+
+var con = mysql.createConnection({
+    user: "root",
+    password: "password",
+    database: "mydb"
+});
+
+app.get('/view-jobs', (req, res) => {
+    con.connect(function(err) {
+        if (err) console.log( err);
+        con.query("SELECT * FROM Jobs INNER JOIN JobSkills on Jobs.idJobs = JobSkills.Jobs_idJobs INNER JOIN JobQualifications on Jobs.idJobs = JobQualifications.Jobs_idJobs", function (err, result) {
+          if (err) {
+            console.log("Error: " + err);
+            return res.status(500).json("Error: " + err);
+          }
+          res.json(result);
+        });
+      });
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/web/dist/web/browser/index.html')
+  })
+  
+
+
+
