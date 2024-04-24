5162be36a192bf86ab8c72792a189cd0f060483d
John Gaskell <john.liam.gaskell@gmail.com>
2024-04-24 01:52:46 +0100

    Created methods to create jobs, view all jobs, and view one job

--- index.js
+++ index.js
@@ -1,44 +1,120 @@
-var http = require('http');
-var fs = require('fs');
-url = require('url');
 express = require('express');
 mysql = require('mysql');
-path = require('path');
 app = express(); 
 
-var publicFolder = path.join(__dirname, './web/dist/web/browser/')
-app.use(express.static(publicFolder))
+var connectionConfig = {
+  connectionLimit : 10,
+  host            : 'localhost',
+  user            : 'root',
+  password        : 'password',
+  database        : 'mydb'
+}
+
+var pool  = mysql.createPool(connectionConfig);
 app.use(express.json());
 
 app.listen(3000, function(err){
     if (err) console.log("Error in server setup")
     console.log("Server listening on Port", 3000);
     
-})
-
-var con = mysql.createConnection({
-    user: "root",
-    password: "password",
-    database: "mydb"
 });
 
 app.get('/get-jobs', (req, res) => {
-    con.connect(function(err) {
+    pool.getConnection(function(err, connection) {
         if (err) console.log( err);
-        con.query("SELECT Jobs.idJobs, Jobs.Company_CompanyName, Jobs.jobDescription, Jobs.payRangeLower, Jobs.jobTitle, Jobs.minimumExperience, Jobs.payRangeUpper, JSON_ARRAYAGG(JobSkills.Skill) AS skills, JSON_ARRAYAGG(JobQualifications.idJobQualifications) AS qualifications FROM Jobs INNER JOIN JobSkills ON Jobs.idJobs = JobSkills.Jobs_idJobs INNER JOIN JobQualifications ON Jobs.idJobs = JobQualifications.Jobs_idJobs GROUP BY Jobs.idJobs;", function (err, result) {
+        connection.query("SELECT Jobs.idJobs, Jobs.Company_CompanyName, Jobs.jobDescription, Jobs.payRangeLower, Jobs.jobTitle, Jobs.minimumExperience, Jobs.payRangeUpper, (SELECT JSON_ARRAYAGG(JobSkills.Skill) from JobSkills where JobSkills.Jobs_idJobs = Jobs.idJobs) AS skills, (SELECT JSON_ARRAYAGG(JobQualifications.idJobQualifications) from jobqualifications where jobqualifications.Jobs_idJobs = Jobs.idJobs) AS qualifications FROM Jobs GROUP BY Jobs.idJobs;", function (err, result) {
           if (err) {
             console.log("Error: " + err);
+            connection.release();
             return res.status(500).json("Error: " + err);
           }
-          res.json(result);
+          res.status(200).json(result);
+          connection.release();
         });
       });
-})
+});
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/web/dist/web/browser/index.html')
-  })
-  
+app.get('/get-job', (req, res) => {
+  pool.getConnection(function(err, connection) {
+      if (err) console.log( err);
+      connection.query(`SELECT Jobs.idJobs, Jobs.Company_CompanyName, Jobs.jobDescription, Jobs.payRangeLower, Jobs.jobTitle, Jobs.minimumExperience, Jobs.payRangeUpper, (SELECT JSON_ARRAYAGG(JobSkills.Skill) from JobSkills where JobSkills.Jobs_idJobs = Jobs.idJobs) AS skills, (SELECT JSON_ARRAYAGG(JobQualifications.idJobQualifications) from jobqualifications where jobqualifications.Jobs_idJobs = Jobs.idJobs) AS qualifications FROM Jobs WHERE Jobs.idJobs = ${req.body.idJobs} GROUP BY Jobs.idJobs;`, function (err, result) {
+        if (err) {
+          console.log("Error: " + err);
+          connection.release();
+          return res.status(500).json("Error: " + err);
+        }
+        res.status(200).json(result);
+        connection.release();
+      });
+    });
+});
+
+app.get('/create-job', (req, res) => {
+
+  pool.getConnection(function(err, connection) {
+      if (err){
+        console.log( err);
+        connection.release();
+        return res.status(500).json("Error: " + err);
+      } 
+      var idJobsFromInput = 0;
+      var sqlSelectCompanyQuery = `SELECT CompanyName FROM Company WHERE CompanyName = '${req.body.Company_CompanyName}';`
+
+      var sqlInsertCompanyQuery = `INSERT INTO Company (CompanyName) VALUES ('${req.body.Company_CompanyName}');`
+      connection.query(sqlSelectCompanyQuery, function (err, result) {
+        if (err) {
+          console.log("Error: " + err);
+          connection.release();
+          return res.status(500).json("Error: " + err);
+        }
+        if(result.length == 0){
+          connection.query(sqlInsertCompanyQuery, function (err, result) {
+            if (err) {
+              console.log("Error: " + err);
+              connection.release();
+              return res.status(500).json("Error: " + err);
+            }
+          });
+        }
+      });
+      var sqlInsertJobQuery = `INSERT INTO Jobs (Company_CompanyName, jobDescription, payRangeLower, jobTitle, minimumExperience, payRangeUpper) VALUES ('${req.body.Company_CompanyName}', '${req.body.jobDescription}', ${req.body.payRangeLower}, '${req.body.jobTitle}', '${req.body.minimumExperience}', ${req.body.payRangeUpper});`
+
+      connection.query(sqlInsertJobQuery, function (err, result) {
+        if (err) {
+          console.log("Error: " + err);
+          connection.release();
+          return res.status(500).json("Error: " + err);
+        }
+
+        idJobsFromInput = result.insertId;
+        var skills = JSON.parse(req.body.skills);
+        var qualifications = JSON.parse(req.body.qualifications);
+        skills.forEach(skill => {
+          var sqlInsertJobSkillsQuery = `INSERT INTO JobSkills (Jobs_idJobs, Skill) VALUES (${idJobsFromInput}, '${skill}');`
+          connection.query(sqlInsertJobSkillsQuery, function (err, result) {
+            if (err) {
+              console.log("Error: " + err);
+              connection.release();
+              return res.status(500).json("Error: " + err);
+            }
+          });
+        });
+        qualifications.forEach(qualification => {
+          var sqlInsertJobQualificationsQuery = `INSERT INTO JobQualifications (Jobs_idJobs, idJobQualifications) VALUES (${idJobsFromInput}, '${qualification}');`
+
+         connection.query(sqlInsertJobQualificationsQuery, function (err, result) {
+            if (err) {
+              console.log("Error: " + err);
+              connection.release();
+              return res.status(500).json("Error: " + err);
+            }
+          });
+        });
+        res.status(200).json({idJobs: idJobsFromInput});
+        connection.release();
+      });
+    });
+});
 
 
 
